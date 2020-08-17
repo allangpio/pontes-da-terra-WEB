743403f9a77c7a67359e312430002acb3b635423
allangpio <gpioallan@gmail.com>
2020-08-17 01:10:25 -0300

    starting to set up the environment

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+interface logo {
+  logo: string;
+}
+
+function Header() {
+  return <h1>Header</h1>;
+}
+
+export default Header;
